de3b15c7fda1dd2c4b3535493b7a1119380b8fe1
Aneesh Gupta <agupta27@ncsu.edu>
2017-03-19 20:14:50 -0400

    first changes

--- basicsearchtree.cc
+++ basicsearchtree.cc
@@ -1,27 +1,45 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 //This is my comment
 
-#define MAX(a,b) (a>b?a:b)
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 
 typedef int key_t;
 typedef char object_t;
-typedef struct text_t {key_t      key; 
-                struct text_t   *left;
-                struct text_t  *right;
-                int height;
-               /* possibly additional information */ } tree_node_t;
+struct m_tree_t {key_t      key; 
+                 struct m_tree_t   *left;
+                 struct m_tree_t  *right;
+                 int height;
+                 int lower_val;
+                 int upper_val;
+                 int leftmin;
+                 int rightmax;
+                 int measure;
+                /* possibly additional information */ };
+
+struct interval_t {
+  int a;
+  int b;
+};
+
+struct interval_list {
+  struct interval_t interval;
+  struct interval_list * next;
+};
 
 #define BLOCKSIZE 256            
-tree_node_t *currentblock = NULL;
+m_tree_t *currentblock = NULL;
 int size_left;
-tree_node_t *free_list = NULL;
+m_tree_t *free_list = NULL;
 int nodes_taken = 0;
 int nodes_returned = 0;
 
-tree_node_t *get_node()
-{ tree_node_t *tmp;
+m_tree_t *get_node()
+{ m_tree_t *tmp;
   nodes_taken += 1;
   if( free_list != NULL )
   {  tmp = free_list;
@@ -30,7 +48,7 @@ tree_node_t *get_node()
   else
   {  if( currentblock == NULL || size_left == 0)
      {  currentblock = 
-                (tree_node_t *) malloc( BLOCKSIZE * sizeof(tree_node_t) );
+                (m_tree_t *) malloc( BLOCKSIZE * sizeof(m_tree_t) );
         size_left = BLOCKSIZE;
      }
      tmp = currentblock++;
@@ -39,52 +57,53 @@ tree_node_t *get_node()
   return( tmp );
 }
 
-void return_node(tree_node_t *node)
+void return_node(m_tree_t *node)
 {  node->right = free_list;
    free_list = node;
    nodes_returned +=1;
 }
 
-void right_rotate (text_t * n) 
+void right_rotate (m_tree_t * n) 
 {
 
-   text_t * tmp = n->right;
+   m_tree_t * tmp = n->right;
+   int tmp_key = n->key;
    n->right = n->left;
+   n->key = n->left->key;
    n->left = n->left->left;
    n->right->left = n->right->right;
    n->right->right = tmp; 
 
-   text_t * x = n->right;
-   x->key = x->left->key + x->right->key;
+   m_tree_t * x = n->right;
+   x->key = tmp_key;
    x->height = 1 + MAX(x->left->height, x->right->height);
 
-   n->key = n->left->key + n->right->key;
    n->height = 1 + MAX(n->left->height, n->right->height);
 }
 
-void left_rotate (text_t * n) 
+void left_rotate (m_tree_t * n) 
 {
-   text_t * tmp = n->left;
+   m_tree_t * tmp = n->left;
+   int tmp_key = n->key;
    n->left = n->right;
+   n->key = n->right->key;
    n->right = n->right->right;
    n->left->right = n->left->left;
    n->left->left = tmp; 
 
-   text_t * x = n->left;
-   x->key = x->left->key + x->right->key;
+   m_tree_t * x = n->left;
+   x->key = tmp_key;
    x->height = 1 + MAX(x->left->height, x->right->height);
-
-   n->key = n->left->key + n->right->key;
+   
    n->height = 1 + MAX(n->left->height, n->right->height);
 
 }
 
-void insert(tree_node_t *tree, key_t new_key, object_t *new_object)
+void insert(m_tree_t *tree, key_t new_key, struct interval_t T)
 {  
     int st_size = 200;
-    //text_t ** stack = (text_t **) calloc(st_size, sizeof(text_t *));
-    text_t * stack[200];
-    tree_node_t *tmp_node;
+    m_tree_t * stack[200];
+    m_tree_t *tmp_node;
     tmp_node = tree;
     int top = -1;
     while( tmp_node->right != NULL ) {
@@ -94,28 +113,33 @@ void insert(tree_node_t *tree, key_t new_key, object_t *new_object)
               return;
           }
           stack[++top] = tmp_node;
-          tmp_node->key = tmp_node->key + 1;
           if( new_key <= tmp_node->left->key ) {
                tmp_node = tmp_node->left;
           } else {               
-               new_key = new_key - tmp_node->left->key;
                tmp_node = tmp_node->right;
           }
     }
     /* found the candidate leaf. Test whether key distinct */ 
     /* key is distinct, now perform the insert */ 
-    tree_node_t *old_leaf, *new_leaf;
+    m_tree_t *old_leaf, *new_leaf;
     old_leaf = get_node();
     old_leaf->left = tmp_node->left; 
     old_leaf->key = tmp_node->key;
     old_leaf->right  = NULL;
     new_leaf = get_node();
-    new_leaf->left = (tree_node_t *) new_object; 
-    new_leaf->key = 1;
-    new_leaf->right  = NULL; 
-    tmp_node->left  = new_leaf;
-    tmp_node->right = old_leaf;
-    tmp_node->key  = 2;
+    new_leaf->left = (m_tree_t *) new_object; 
+    new_leaf->key = new_key;
+    new_leaf->right  = NULL;
+
+    if (tmp_node->key < new_key ) {
+        tmp_node->left = old_leaf;
+        tmp_node->right = new_leaf;
+        tmp_node->key = new_key;
+    } else {     
+        tmp_node->left = new_leaf;
+        tmp_node->right = old_leaf;
+    }
+
     //set the heights of the nodes
     tmp_node->height = 1;
     new_leaf->height = 0;
@@ -143,16 +167,15 @@ void insert(tree_node_t *tree, key_t new_key, object_t *new_object)
         }
         if(tmp_node->height == prev_height) break;
   }
-  //free(stack);
 }
 
-object_t *_delete(tree_node_t *tree, key_t delete_key)
-{  tree_node_t *tmp_node, *upper_node, *other_node;
+object_t *_delete(m_tree_t *tree, key_t delete_key, struct interval_t T)
+{  m_tree_t *tmp_node, *upper_node, *other_node;
    
    object_t *deleted_object;
    int st_size = 200;
-   //text_t ** stack = (text_t **) calloc(st_size, sizeof(text_t *));
-   text_t * stack[200];
+   //m_tree_t ** stack = (m_tree_t **) calloc(st_size, sizeof(m_tree_t *));
+   m_tree_t * stack[200];
    int top = -1;
    
    if (tree->key == 1 || delete_key >= tree->key) {
@@ -166,14 +189,12 @@ object_t *_delete(tree_node_t *tree, key_t delete_key)
               return (NULL);
           }
           stack[++top] = tmp_node;   
-          tmp_node->key = tmp_node->key - 1;
           upper_node = tmp_node;
           
           if(delete_key <= tmp_node->left->key ) {  
               tmp_node   = upper_node->left; 
               other_node = upper_node->right;
           } else { 
-               delete_key = delete_key - upper_node->left->key;
                tmp_node   = upper_node->right; 
                other_node = upper_node->left;
           } 
@@ -219,9 +240,87 @@ object_t *_delete(tree_node_t *tree, key_t delete_key)
     }
 }
 
-void level_order (tree_node_t *tree) {
-    text_t * queue[200];
-    text_t * tmp = NULL;
+
+// the main functions
+
+m_tree_t * create_m_tree() {
+  
+  m_tree_t * new_tree;
+  new_tree = get_node();
+  
+  new_tree->left = NULL;
+  new_tree->right = NULL;
+  new_tree->height = 0;
+  new_tree->measure = 0;
+  new_tree->leftmin = INT_MIN;
+  new_tree->rightmax = INT_MAX;
+  
+  return ( new_text );
+}
+
+int query_length(m_tree_t *tree) {
+  /* returns the measure of the current intervals set. */
+   if (tree == NULL) return -1;
+   return (tree->measure); 
+}
+
+void insert_interval( m_tree_t *tree, int lower, int upper) {
+  /* inserts the interval into the tree
+  */
+    if (tree == NULL || lower >= upper) return;
+    
+    struct interval_t y;
+    y.a = lower;
+    y.b = upper;
+
+    insert(tree, lower,  y);
+    insert(tree, upper,  y);
+}
+
+void delete_interval( m_tree_t *tree, int lower, int upper) {
+  /* deletes the interval from the tree
+  */
+  if (tree == NULL || lower >= upper) return NULL;
+  
+  struct interval_t y;
+  y.a = lower;
+  y.b = upper;
+
+  _delete(tree, lower, y); 
+  _delete(tree, upper, y); 
+}
+
+void destroy_m_tree(m_tree_t *tree)
+{  m_tree_t *current_node, *tmp;
+   if( tree->left == NULL )
+      return_node( tree );
+   else
+   {  current_node = tree;
+      while(current_node->right != NULL )
+      {  if( current_node->left->right == NULL )
+         {  return_node( current_node->left );
+            tmp = current_node->right;
+            return_node( current_node );
+            current_node = tmp;
+         }
+         else
+         {  tmp = current_node->left;
+            current_node->left = tmp->right;
+            tmp->right = current_node; 
+            current_node = tmp;
+         }
+      }
+      return_node( current_node );
+   }
+}
+
+/*
+ Functions to Test
+ */
+
+void level_order (m_tree_t *tree) {
+    m_tree_t * queue[200];
+    m_tree_t * tmp = NULL;
     int i = 0;
     queue[0] = tree;
     queue[1] = NULL;
@@ -248,38 +347,10 @@ void level_order (tree_node_t *tree) {
     }
 }
 
-void remove_tree(tree_node_t *tree)
-{  tree_node_t *current_node, *tmp;
-   if( tree->left == NULL )
-      return_node( tree );
-   else
-   {  current_node = tree;
-      while(current_node->right != NULL )
-      {  if( current_node->left->right == NULL )
-         {  return_node( current_node->left );
-            tmp = current_node->right;
-            return_node( current_node );
-            current_node = tmp;
-         }
-         else
-         {  tmp = current_node->left;
-            current_node->left = tmp->right;
-            tmp->right = current_node; 
-            current_node = tmp;
-         }
-      }
-      return_node( current_node );
-   }
-}
-
-text_t * create_text();
-void insert_line( text_t *txt, int index, char * new_line);
-char * get_line( text_t *txt, int index);
-
 int main()
 {  
    
-   tree_node_t *searchtree;
+   m_tree_t *searchtree;
    char nextop;
    searchtree = create_text();
    printf("Made Tree\n");
@@ -294,7 +365,7 @@ int main()
        scanf ("%[^\n]%*c", strobj);
        insert_line( searchtree, inskey, strobj );
        printf("  insert line successful, key = %d, object value = %s, \n",
-        	  inskey, strobj);
+            inskey, strobj);
        level_order(searchtree);
      }  
      if(nextop == 'f' ) { 
@@ -324,96 +395,6 @@ int main()
    remove_tree( searchtree );
    printf("Removed tree.\n");
    printf("Total number of nodes taken %d, total number of nodes returned %d\n",
-	  nodes_taken, nodes_returned );
+    nodes_taken, nodes_returned );
    return(0);
-}
-
-// the main functions
-
-text_t * create_text() {
-  text_t * new_text;
-  new_text = get_node();
-  new_text->left = ( text_t *) ("\0");
-  new_text->key = 1;
-  new_text->right = NULL;
-  new_text->height = 0;
-  return ( new_text );
-}
-int length_text( text_t *txt) {
-  /* returns the number of lines of the current text. */
-   if (txt == NULL) return -1;
-   return (txt->key - 1); 
-}
-
-char * get_line( text_t *txt, int index) {
-  /* gets the line of number index, if such a line exists, and
-    returns NULL else.
-  */
-  if (txt == NULL || txt->key == 1 || index >= txt->key) {
-     return NULL;
-  }
-  //printf("root height is %d key is %d\n",txt->height, txt->key);
-  text_t *temp = txt;
-  // later test and change to temp->key == 1 !!!!!!!!!!!!!!!! change to for ??
-  while (temp->right != NULL){
-      if (index > temp->left->key) {
-          index = index - temp->left->key;
-          temp = temp->right;
-      } else {
-          temp = temp->left;
-     }
-  }
-  return (char *) temp->left;
-}
-
-void append_line( text_t *txt, char * new_line) {
-  /* appends new line as new last line. */
-  if (txt == NULL) return;
-  insert(txt, txt->key, new_line);
-
-}
-
-char * set_line( text_t *txt, int index, char * new_line) {
-  /* sets the line of number index, if such
-    a line exists, to new line, and returns a pointer to the previous line of that number. If no line of
-    that number exists, it does not change the structure and returns NULL.
-  */
-    if (txt == NULL || txt->key == 1 || index >= txt->key) {
-     return NULL;
-  }
-  text_t *temp = txt;
-  // later test and change to temp->key == 1 !!!!!!!!!!!!!!!! change to for ??
-  while (temp->right != NULL){
-      if (index > temp->left->key) {
-          index = index - temp->left->key;
-          temp = temp->right;
-      } else {
-          temp = temp->left;
-     }
-  }
-  char * old  = (char *) temp->left;
-  temp->left = (text_t *) new_line;
-  return old;
-}
-
-void insert_line( text_t *txt, int index, char * new_line) {
-  /* inserts the line before the line of
-     number index, if such a line exists, to new line, renumbering all lines after that line. If no such
-    line exists, it appends new line as new last line.
-  */
-    if (txt == NULL) return;
-    if (index >= txt->key) {
-        append_line(txt, new_line);
-    } else {
-        insert(txt, index, new_line);
-    }
-}
-
-char * delete_line( text_t *txt, int index) {
-  /* deletes the line of number index, renumbering all
-    lines after that line, and returns a pointer to the deleted line.
-  */
-  if (txt == NULL) return NULL;
-  
-  return _delete(txt, index);  
 }
